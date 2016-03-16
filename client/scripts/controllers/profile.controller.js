40d0e7b96bb6fd1cba2cc15b86d5a163f4f3eed7
Christoffer Jahren <christoffer@jahren.as>
2016-03-16 14:01:49 +0100

    updated profile view

--- client/scripts/controllers/profile.controller.js
+++ client/scripts/controllers/profile.controller.js
@@ -8,6 +8,15 @@ function ProfileCtrl($scope, $reactive) {
 	$scope.user = {
     first_name: "Marvin",
     description: "Mr. AI",
-    image_url: "http://m.eet.com/media/1195785/Marvin-Minsky-500.jpg"
+    image_url: "http://m.eet.com/media/1195785/Marvin-Minsky-500.jpg",
+		activity: {
+			daily_step_count: 1500,
+			daily_step_goal: 2000,
+			weekly_step_count: 5750,
+			weekly_step_goal: 6000
+		}
   }
+
+	$scope.daily_step_percent = ($scope.user.activity.daily_step_count / $scope.user.activity.daily_step_goal) * 100
+	$scope.weekly_step_percent = ($scope.user.activity.weekly_step_count / $scope.user.activity.weekly_step_goal) * 100
 }
